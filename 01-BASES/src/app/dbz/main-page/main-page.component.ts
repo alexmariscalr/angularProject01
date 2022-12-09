b15a72d407ff67a74ea640613a1561f68e4a9f71
Alejandro Mariscal Romero <alejandro.mariscal.romero@alumnos.upm.es>
2022-12-09 08:30:38 +0100

    Character List

--- 01-BASES/src/app/dbz/main-page/main-page.component.ts
+++ 01-BASES/src/app/dbz/main-page/main-page.component.ts
@@ -12,16 +12,41 @@ interface Personaje {
 })
 export class MainPageComponent {
 
+  personajes : Personaje [] = [ 
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 7500
+    }
+  ]
+
   nuevo: Personaje = {
-    nombre: 'Trucks',
-    poder: 14000
+    nombre: '',
+    poder: 0
   }
 
   
 
   agregar() {
-    console.log(this.nuevo)
+    if (this.nuevo.nombre.trim().length === 0) {
+      return;
+    }
+
+    this.personajes.push(this.nuevo);
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
+
+    console.log( this.nuevo );
+
+    //this.personajes.push(...)
+    // this.nuevo
   }
+
 
 
 }
