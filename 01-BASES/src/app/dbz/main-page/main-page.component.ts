9fefb464bd23ed223e16bea6641c9e92d44bac54
Alejandro Mariscal Romero <alejandro.mariscal.romero@alumnos.upm.es>
2022-12-12 08:45:38 +0100

    Agregar component

--- 01-BASES/src/app/dbz/main-page/main-page.component.ts
+++ 01-BASES/src/app/dbz/main-page/main-page.component.ts
@@ -21,29 +21,9 @@ export class MainPageComponent {
   ]
 
   nuevo: Personaje = {
-    nombre: '',
-    poder: 0
+    nombre: 'Maestro Roshi',
+    poder: 1000
   }
-
-  
-
-  agregar() {
-    if (this.nuevo.nombre.trim().length === 0) {
-      return;
-    }
-
-    this.personajes.push(this.nuevo);
-    this.nuevo = {
-      nombre: '',
-      poder: 0
-    }
-
-    console.log( this.nuevo );
-
-    //this.personajes.push(...)
-    // this.nuevo
-  }
-
 
 
 }
