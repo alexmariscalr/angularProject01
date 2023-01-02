8a6fb3d4c3b82470fbed97f436cbb9c2ca59a6bb
Alejandro Mariscal Romero <amariscalr@indra.es>
2023-01-02 16:04:24 +0100

    sidebar

--- 03-paisesApp/src/app/shared/shared.module.ts
+++ 03-paisesApp/src/app/shared/shared.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [SidebarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     SidebarComponent
