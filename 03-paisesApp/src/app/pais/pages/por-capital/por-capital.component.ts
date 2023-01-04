1d8e64c466edba1a7ec1cf3b0a20b8708fb9a5ac
Alejandro Mariscal Romero <amariscalr@indra.es>
2023-01-04 09:08:04 +0100

    porCapital

--- 03-paisesApp/src/app/pais/pages/por-capital/por-capital.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-capital',
@@ -8,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorCapitalComponent implements OnInit {
 
-  constructor() { }
+  termino: string = '';
+  hayError:boolean = false;
+  paises: Country[] = []
 
-  ngOnInit(): void {
+ constructor(private paisService: PaisService) {}
+
+  buscar( termino: string) {
+    this.hayError = false;
+    this.paisService.buscarCapital(termino)
+    .subscribe( (paises) => {
+      this.paises = paises
+    } )
   }
+
+ ngOnInit(): void {
+ }
 
 }
