79462916a1a346bf869c94fd441976ec0d0e3f74
Alejandro Mariscal Romero <amariscalr@indra.es>
2023-01-04 16:45:41 +0100

    primeNg

--- 04-pipesApp/src/app/app.component.ts
+++ 04-pipesApp/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipeApp';
+ 
+  nombre : string = 'Alejandro'
+
+  mostrarNombre() {
+    console.log(this.nombre)
+  }
 }
