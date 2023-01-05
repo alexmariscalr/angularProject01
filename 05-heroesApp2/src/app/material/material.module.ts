c6b1ae4604dc47ed6463772882819268e11dbbc3
Alejandro Mariscal Romero <amariscalr@indra.es>
2023-01-05 13:45:42 +0100

    Flex - Diferentes resoluciones

--- 05-heroesApp2/src/app/material/material.module.ts
+++ 05-heroesApp2/src/app/material/material.module.ts
@@ -6,7 +6,7 @@ import {MatListModule} from '@angular/material/list';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
-
+import {MatGridListModule} from '@angular/material/grid-list';
 
 
 
@@ -20,7 +20,8 @@ import {MatCardModule} from '@angular/material/card';
     MatButtonModule,
     MatIconModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatGridListModule
   ]
 })
 export class MaterialModule { }
