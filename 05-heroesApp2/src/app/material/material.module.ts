5bb3a912760b3f1c89613fb6314e52f4842e5585
Alejandro Mariscal Romero <amariscalr@indra.es>
2023-01-05 12:20:31 +0100

    material navlist

--- 05-heroesApp2/src/app/material/material.module.ts
+++ 05-heroesApp2/src/app/material/material.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
@@ -15,7 +16,8 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     MatSidenavModule,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatListModule
   ]
 })
 export class MaterialModule { }
