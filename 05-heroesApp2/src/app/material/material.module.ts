0a1bfe147c6889a0a8465475fb2c8b5c1ba7129c
Alejandro Mariscal Romero <amariscalr@indra.es>
2023-01-05 13:17:37 +0100

    Material Card - Flex Layout

--- 05-heroesApp2/src/app/material/material.module.ts
+++ 05-heroesApp2/src/app/material/material.module.ts
@@ -5,6 +5,8 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+
 
 
 
@@ -17,7 +19,8 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
